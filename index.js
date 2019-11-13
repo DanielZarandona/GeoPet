417ccb413a2fc90494a8d7ef312fc87a5817f1a8
Daniel Ortiz Zarandona <daozara@gmail.com>
2019-11-13 16:00:30 -0600

    Marker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
+ 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -15,6 +15,7 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
